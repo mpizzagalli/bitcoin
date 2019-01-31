486b77a8da1a2cbb0c9da8e91a6d6aaa7a89daae
ndecarli <ndecarli@dc.uba.ar>
2019-01-31 16:18:11 -0300

    Add config for 300s test

--- testEngine.go
+++ testEngine.go
@@ -18,8 +18,8 @@ import (
 )
 
 const txFee float64 = 0.000002
-const txSleepMaxAggregate = int64(time.Millisecond * 4570)
-const txSleepMinimum = time.Second * 16
+const txSleepMaxAggregate = int64(time.Millisecond * 9140)
+const txSleepMinimum = time.Second * 32
 
 type UnspentOutput struct {
 	Address string `json:"address"`
@@ -86,7 +86,7 @@ func mineBlocks(addresses []string) {
 	timestamp := time.Now().UnixNano()
 
 	for i := 0;;i ^= 1 {
-		sleepTime = (rng.ExpFloat64() / simuLambda)*150.0
+		sleepTime = (rng.ExpFloat64() / simuLambda)*300.0
 		sleepSeconds = time.Duration(sleepTime)
 		sleepNanoseconds = time.Duration((sleepTime-float64(sleepSeconds))*1000000000.0)
 		nextBlockTime = time.Duration(timestamp) + sleepSeconds * time.Second + sleepNanoseconds
