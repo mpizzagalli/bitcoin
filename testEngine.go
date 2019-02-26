a1212936ed2a1806d8a55792646004817020ed2d
ndecarli <ndecarli@dc.uba.ar>
2019-02-26 18:17:05 -0300

    Adjust sleep to 6000tx/bloque

--- testEngine.go
+++ testEngine.go
@@ -18,8 +18,8 @@ import (
 )
 
 const txFee float64 = 0.000002
-const txSleepMaxAggregate = int64(time.Millisecond * 4518)
-const txSleepMinimum = time.Millisecond * 33882
+const txSleepMaxAggregate = int64(time.Millisecond * 2003)
+const txSleepMinimum = time.Millisecond * 23040
 
 type UnspentOutput struct {
 	Address string `json:"address"`
